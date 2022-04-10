31765601398359fa4ca2db245108cffb96912f21
sthav <sthavidhr@gmail.com>
2022-04-11 01:52:37 +0530

    add the name it project

---
+++ src/components/App/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { ReactDOM } from "react";
+import Header from "../Header/Header";
+import ResultsContainer from "../ResultsContainer/ResultsContainer";
+import SearchBox from "../SearchBox/SearchBox";
+import './App.css';
+
+const name = require('@rstacruz/startup-name-generator');
+
+
+const App = () => {
+
+  const [headerText, setheaderText] = useState('Name It!');
+  const [headerExpanded, setheaderExpanded] = useState(true);
+  const [suggestedNames, setSuggestedNames] = useState([]);
+
+  const handleInputChange = (inputText) => {
+     setheaderExpanded(!inputText);
+     setSuggestedNames(inputText ? name(inputText) :[]); 
+    
+  };
+
+  return (
+    <div>
+      <Header headTitle={headerText} headerExpanded={headerExpanded} /> 
+      <SearchBox onInputChange={handleInputChange}/>
+      <ResultsContainer suggestedNames={suggestedNames}/>
+    </div>
+  );
+}
+
+export default App;  
